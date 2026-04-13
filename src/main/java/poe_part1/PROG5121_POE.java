03545ef88ff9f7104c46b2c747e1c89b5839fe21
st10499467 <heldsingerc@gmail.com>
2026-04-13 12:54:47 +0200

    Initial commit

---
+++ src/main/java/poe_part1/PROG5121_POE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poe_part1;
+
+/**
+ *
+ * @author Student
+ */
+public class PROG5121_POE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
