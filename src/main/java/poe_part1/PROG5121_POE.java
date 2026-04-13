1346d5d575c40c17d490bc61293cc4b881120328
st10499467 <heldsingerc@gmail.com>
2026-04-13 15:24:24 +0200

    main logic and scanner added

--- src/main/java/poe_part1/PROG5121_POE.java
+++ src/main/java/poe_part1/PROG5121_POE.java
@@ -1,16 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package poe_part1;
 
-/**
- *
- * @author Student
- */
+import java.util.Scanner;
+
 public class PROG5121_POE {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        Login auth = new Login();
+
+        System.out.println("--- Registration ---");
+
+        System.out.print("First Name: ");
+        String fName = sc.nextLine();
+
+        System.out.print("Last Name: ");
+        String lName = sc.nextLine();
+        auth.setNames(fName, lName);
+
+        System.out.print("Username: ");
+        String user = sc.nextLine();
+
+        System.out.print("Password: ");
+        String pass = sc.nextLine();
+
+        // Register and show result
+        String result = auth.registerUser(user, pass);
+        System.out.println("\n" + result);
+
+        // Login phase
+        if (result.equals("Username and password successfully captured.")) {
+            System.out.println("\n--- Login ---");
+            System.out.print("Enter Username: ");
+            String logUser = sc.nextLine();
+
+            System.out.print("Enter Password: ");
+            String logPass = sc.nextLine();
+
+            boolean isOk = auth.loginUser(logUser, logPass);
+            System.out.println(auth.returnLoginStatus(isOk));
+        }
     }
 }
