89a1d4706f62a372c0617b9d7090963e119da52e
st10499467 <heldsingerc@gmail.com>
2026-05-06 17:30:52 +0200

    Update Login.java

--- src/main/java/poe_part1/Login.java
+++ src/main/java/poe_part1/Login.java
@@ -7,56 +7,65 @@
  */
 package poe_part1;
 
+import javax.swing.JOptionPane;
+
 public class Login {
 
+    
+    private String regFirst;
+    private String regLast;
     private String regUser;
     private String regPass;
-    private String fName;
-    private String lName;
 
-   
+    
     public boolean checkUserName(String username) {
         return username.contains("_") && username.length() <= 5;
     }
 
    
     public boolean checkPasswordComplexity(String password) {
-        String pattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$";
-        return password.matches(pattern);
-        // The following regex logic for password complexity was adapted from:
+        boolean hasUpper = false;
+        boolean hasNum = false;
+        boolean hasSpecial = false;
 
-
+        if (password.length() >= 8) {
+            for (char c : password.toCharArray()) {
+                if (Character.isUpperCase(c)) hasUpper = true;
+                if (Character.isDigit(c)) hasNum = true;
+                if (c == '!' || c == '@' || c == '#' || c == '$' || c == '%' || c == '^' || c == '&' || c == '*')
+                    hasSpecial = true;
+            }
+        }
+        return hasUpper && hasNum && hasSpecial;
     }
 
     
-    public String registerUser(String username, String password) {
-        if (!checkUserName(username)) {
-            return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
-        }
-        if (!checkPasswordComplexity(password)) {
-            return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
-        }
+    public String registerUser(String firstName, String lastName, String username, String password) {
+        this.regFirst = firstName;
+        this.regLast = lastName;
 
-        this.regUser = username;
-        this.regPass = password;
-        return "Username and password successfully captured.";
+        if (!checkUserName(username)) {
+            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
+        } else if (!checkPasswordComplexity(password)) {
+            return "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
+        } else {
+            this.regUser = username;
+            this.regPass = password;
+            return "Username and password successfully captured.";
+        }
     }
 
     
     public boolean loginUser(String username, String password) {
-        return username.equals(regUser) && password.equals(regPass);
+        return username.equals(this.regUser) && password.equals(this.regPass);
     }
 
     
-    public String returnLoginStatus(boolean loggedIn) {
-        if (loggedIn) {
-            return "Welcome " + fName + ", " + lName + " it is great to see you again.";
+    public String returnLoginStatus(boolean isLoggedIn) {
+        if (isLoggedIn) {
+            return "Welcome " + regFirst + ", " + regLast + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again";
         }
-        return "Username or password incorrect, please try again.";
-    }
-
-    public void setNames(String first, String last) {
-        this.fName = first;
-        this.lName = last;
     }
 }
